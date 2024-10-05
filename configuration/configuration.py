9f06f1a3d9a76f7ab7fee39501f0a0d34c29f8e1
dkraklan <dylan@dkraklan.me>
2024-10-05 10:38:14 -0700

    Remove unused default plugin

--- configuration/configuration.py
+++ configuration/configuration.py
@@ -36,12 +36,8 @@ DEVELOPER = os.environ.get("DEVELOPER", "true")
 INTERNAL_IPS = ("0.0.0.0", "127.0.0.1", "::1")
 
 PLUGINS = [
-    "netbox_acls",
 ]
 PLUGINS_CONFIG = {
-        "netbox_acls": {
-            "top_level_menu": True
-            }
 }
 
 SCRIPTS_ROOT = "scripts"
